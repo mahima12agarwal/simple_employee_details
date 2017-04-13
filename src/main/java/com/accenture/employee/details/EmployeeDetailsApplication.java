e8d53874ecb3c4100f564a055f5e42a104f6e395
abhijit.panchal <abhijit.panchal@accenture.com>
2017-04-13 10:56:03 +0530

    modified pom and commented extra annotations

--- src/main/java/com/accenture/employee/details/EmployeeDetailsApplication.java
+++ src/main/java/com/accenture/employee/details/EmployeeDetailsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableCircuitBreaker
+//@EnableDiscoveryClient
+//@EnableCircuitBreaker
 /**
  * 
  * @author j.venugopalan
