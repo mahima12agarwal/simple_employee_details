9bcb06342f28a5aa38ca5d4db53a2d46c5464c28
abhijit.panchal <abhijit.panchal@accenture.com>
2017-04-13 11:12:11 +0530

    corrected import statememnts

--- src/main/java/com/accenture/employee/details/EmployeeDetailsApplication.java
+++ src/main/java/com/accenture/employee/details/EmployeeDetailsApplication.java
@@ -2,8 +2,8 @@ package com.accenture.employee.details;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 //@EnableDiscoveryClient
