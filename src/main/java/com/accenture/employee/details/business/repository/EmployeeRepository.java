783ce77296ab36e4a2f8a1efa62ae7ab0b7af3a0
abhijit.panchal <abhijit.panchal@accenture.com>
2017-05-08 20:13:51 +0530

    added Hystrix related implementation

--- src/main/java/com/accenture/employee/details/business/repository/EmployeeRepository.java
+++ src/main/java/com/accenture/employee/details/business/repository/EmployeeRepository.java
@@ -1,7 +1,5 @@
 package com.accenture.employee.details.business.repository;
 
-import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.accenture.employee.details.business.vo.Employee;
